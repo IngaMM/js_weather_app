f39058c82191d00cf4bad72c173cd66bc37a3312
inga mahle <inga.mahle@gmx.de>
2020-03-09 13:55:15 +0630

    Styling improved

--- README.md
+++ README.md
@@ -10,8 +10,10 @@ Instructions
 2. Open index.html with a browser.
 
 Discussion
-I used the following technologies: HTML, CSS and Javascript with an external API.
-The weather forecastsite uses this weather API (https://openweathermap.org).
+I used the following technologies: HTML (ES6), CSS and Javascript. The weather data are requested from an external API (https://openweathermap.org). The fetch command which gets back a promise is used to request the data from this API. The then-syntax is used to deal with the promise. Multiple "thens" are chained and error handling with catch is included.
+DOM manipulation allows the display of a loading message while the request to the external API is in progress.
+When the weather data are received successully DOM manipulations makes the loading message disappear and shows the data. When there is an error an error message is shown instead.
+The background image is chosen in dependance of the weather forecast. The website is styled in a way that it can be viewed on devices with different screen sizes. Vendor prefixes assure that the website can be seen correctly on different browsers.
 
 Requirements
 Browser, internet access
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,15 @@
-const loading = document.getElementById("loading");
-const title = document.getElementById("title");
-const description = document.getElementById("description");
-const img = document.querySelector("img");
-const temperature = document.getElementById("temperature");
-const conversionButtonCtoF = document.getElementById("conversionButtonCtoF");
-const conversionButtonFtoC = document.getElementById("conversionButtonFtoC");
+const getWeatherData = () => {
+  const loading = document.getElementById("loading");
+  const title = document.getElementById("title");
+  const description = document.getElementById("description");
+  const img = document.querySelector("img");
+  const temperature = document.getElementById("temperature");
+  const conversionButtonCtoF = document.getElementById("conversionButtonCtoF");
+  const conversionButtonFtoC = document.getElementById("conversionButtonFtoC");
 
-function getWeatherData() {
-  errorMessage.classList.add("invisible");
-  loading.classList.remove("invisible");
-  const city = document.getElementById("city").value;
+  errorMessage.classList.add("invisible"); // Do not show error message
+  loading.classList.remove("invisible"); // Show loading message
+  const city = document.getElementById("city").value; // Get city name from input field
   const url =
     "http://api.openweathermap.org/data/2.5/weather?APPID=1fc3358fe4cd05550f79f25b6ec4e4e0&q=" +
     city;
@@ -18,6 +18,7 @@ function getWeatherData() {
       return response.json();
     })
     .then(function(response) {
+      // Fill weather data object
       weatherData = {};
       weatherData.description = response.weather[0].description;
       weatherData.tempC = response.main.temp - 273.15;
@@ -26,43 +27,108 @@ function getWeatherData() {
         "http://openweathermap.org/img/wn/" +
         response.weather[0].icon +
         "@2x.png";
-      loading.classList.add("invisible");
-      title.innerHTML = "The current weather in " + city + ":";
-      let description_array = weatherData.description.split(" ");
-      // Capitalize first letter
-      let description_array_cap = description_array.map(
+
+      loading.classList.add("invisible"); // Do not show loading message any more
+      title.textContent = "The current weather in " + city + ":";
+
+      // Format & set description and image
+      let descriptionArray = weatherData.description.split(" ");
+      let descriptionArrayCap = descriptionArray.map(
         word => word.charAt(0).toUpperCase() + word.slice(1)
       );
-      description.innerHTML = description_array_cap.join(" ");
+      description.textContent = descriptionArrayCap.join(" ");
       img.src = weatherData.iconURL;
-      temperature.innerHTML =
+
+      // Give temperature in Celsius and show conversion button to F
+      temperature.textContent =
         "Temperature [°C]: " + weatherData.tempC.toFixed(1);
       conversionButtonFtoC.classList.add("invisible");
       conversionButtonCtoF.classList.remove("invisible");
+
+      // Set background image and font color in dependance of weather icon
+      setBackgroundImageAndColor(response.weather[0].icon);
     })
     .catch(function(error) {
+      // Show error message and default background & font color
       loading.classList.add("invisible");
       errorMessage.classList.remove("invisible");
-      errorMessage.innerHTML = "City not found.";
-      title.innerHTML = "";
-      description.innerHTML = "";
+      errorMessage.textContent = "City not found.";
+      title.textContent = "";
+      description.textContent = "";
       img.src = "";
-      temperature.innerHTML = "";
+      temperature.textContent = "";
+      setBackgroundImageAndColor("");
       conversionButtonFtoC.classList.add("invisible");
       conversionButtonCtoF.classList.add("invisible");
-
       console.log(error);
     });
-}
+};
 
-function convertCtoF() {
+// Conversion function for C to F
+const convertCtoF = () => {
   conversionButtonCtoF.classList.add("invisible");
   conversionButtonFtoC.classList.remove("invisible");
-  temperature.innerHTML = "Temperature [°F]: " + weatherData.tempF.toFixed(1);
-}
+  temperature.textContent = "Temperature [°F]: " + weatherData.tempF.toFixed(1);
+};
 
-function convertFtoC() {
+// Conversion function for F to C
+const convertFtoC = () => {
   conversionButtonFtoC.classList.add("invisible");
   conversionButtonCtoF.classList.remove("invisible");
-  temperature.innerHTML = "Temperature [°C]: " + weatherData.tempC.toFixed(1);
-}
+  temperature.textContent = "Temperature [°C]: " + weatherData.tempC.toFixed(1);
+};
+
+// Function to set background image and font color in dependance of weather icon
+const setBackgroundImageAndColor = icon => {
+  let color;
+  let backgroundImage;
+  switch (icon) {
+    case "01d":
+      backgroundImage = "url('./assets/pictures/clear_day.jpg')";
+      color = "black";
+      break;
+    case "01n":
+      backgroundImage = "url('./assets/pictures/clear_night.jpg')";
+      color = "white";
+      break;
+    case "02d":
+    case "03d":
+    case "04d":
+      backgroundImage = "url('./assets/pictures/clouds_day.jpg')";
+      color = "black";
+      break;
+    case "02n":
+    case "03n":
+    case "04n":
+      backgroundImage = "url('./assets/pictures/clouds_night.jpg')";
+      color = "white";
+      break;
+    case "09d":
+    case "09n":
+    case "10d":
+    case "10n":
+      backgroundImage = "url('./assets/pictures/rain.jpg')";
+      color = "white";
+      break;
+    case "11d":
+    case "11n":
+      backgroundImage = "url('./assets/pictures/thunderstorm.jpg')";
+      color = "white";
+      break;
+    case "13d":
+    case "13n":
+      backgroundImage = "url('./assets/pictures/snow.jpg')";
+      color = "white";
+      break;
+    case "50d":
+    case "50n":
+      backgroundImage = "url('./assets/pictures/snow.jpg')";
+      color = "black";
+      break;
+    default:
+      backgroundImage = "url('./assets/pictures/default_background.jpg')";
+      color = "black";
+  }
+  document.body.style.backgroundImage = backgroundImage;
+  document.body.style.color = color;
+};
